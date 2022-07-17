5a03052aaca01008e37858b4fd40a227fb5a4155
Ian Littlewood <ian.littlewood@ansaldonuclear.com>
2022-07-17 17:11:26 +0100

    1st

---
+++ BuildAssemblyGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Microsoft.Build.Utilities;
+using System.Xml;
+using Microsoft.Build.Framework;
+using System;
+using System.Reflection;
+using System.Linq;
+
+namespace SoupSoftware
+{
+    public class BuildAssemblyGenerator : Task
+    {
+
+
+        [Required]
+        public string year { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+     
+        public override bool Execute()
+        {
+            bool cont = true;
+            Log.LogMessage(MessageImportance.High, "Starting Assembly Version Generation", null);
+            try
+            {
+                int yearasInt = Convert.ToInt32(year);
+                string assyversion = (yearasInt - 2007).ToString() + ".0";
+                string[] CodeStr = new string[] { };
+
+                if (FileName.ToLower().Contains("vb")){ 
+                CodeStr = APIStravb(assyversion);
+                }
+                else if (FileName.ToLower().Contains("cs"))
+                {
+                    
+                CodeStr = APIStravc(assyversion);
+                }
+                foreach (string c in System.IO.Path.GetInvalidFileNameChars().Intersect(System.IO.Path.GetInvalidPathChars()).Select(x => x.ToString()).ToArray())
+                {
+                    FileName = FileName.Replace(c, "");
+                }
+                string Path = System.IO.Path.GetDirectoryName(FileName);
+                string fn = System.IO.Path.GetFileName(FileName).TrimStart().TrimEnd();
+                FileName = System.IO.Path.Combine(Path, fn);
+
+                System.IO.File.WriteAllLines(
+                   FileName, CodeStr
+
+                   );
+
+            }
+
+
+            catch (Exception ex)
+            {
+                Log.LogError(ex.Message);
+            }
+             return cont;
+        }
+
+        private static string[] APIStravc(string assyversion)
+        {
+            List<string> line = new List<string>();
+            line.Add(string.Format("[assembly: Autodesk.Connectivity.Extensibility.Framework.ApiVersion({0})]",
+               new string[] { char.ConvertFromUtf32(34) + assyversion + char.ConvertFromUtf32(34) }));
+            return line.ToArray();
+        }
+        private static string[] APIStravb(string assyversion)
+        {
+            List<string> line = new List<string>();
+            
+            line.Add(string.Format("<Assembly: Autodesk.Connectivity.Extensibility.Framework.ApiVersion({0})>",
+               new string[] { char.ConvertFromUtf32(34) + assyversion + char.ConvertFromUtf32(34)}));
+            return line.ToArray();
+        }
+    }
+}
+
